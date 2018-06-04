6a65c26e304820bc601e9664d06f4ab72c3fe373
Chris Penner <cpenner@vendasta.com>
2018-06-04 15:57:48 -0600

    Simple initial implementation with single component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SearchBoxComponent } from './search-box/search-box.component';
-import { RepositoryListComponent } from './repository-list/repository-list.component';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule, MatInputModule } from '@angular/material';
 import { RepositoryService } from './repository.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, SearchBoxComponent, RepositoryListComponent],
+  declarations: [AppComponent, SearchBoxComponent],
   imports: [BrowserModule, MatInputModule, MatFormFieldModule, FormsModule, HttpClientModule],
   providers: [RepositoryService],
   bootstrap: [AppComponent],
---
+++ src/app/repository.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+
+export interface Repository {
+  name: string;
+  author: string;
+  url: string;
+  description: string;
+  topics: string[];
+}
+
+const githubApiUrl = `https://api.github.com`;
+const githubSearchRepoPath = `/search/repositories`;
+
+@Injectable()
+export class RepositoryService {
+  constructor(private httpClient: HttpClient) {}
+
+  searchRepositories(topic: string): Observable<Repository[]> {
+    const params = { q: `topic:${topic}` };
+    return this.httpClient
+      .get(githubApiUrl + githubSearchRepoPath, {
+        headers: { Accept: 'application/vnd.github.mercy-preview+json' },
+        params,
+      })
+      .pipe(
+        map((response: any) => response.items),
+        map(repos =>
+          repos.map(repo => ({
+            name: repo.name,
+            author: repo.owner.login,
+            url: repo.html_url,
+            description: repo.description,
+            topics: repo.topics,
+          })),
+        ),
+      );
+  }
+}
