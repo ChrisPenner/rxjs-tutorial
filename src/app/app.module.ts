94e1ad3af5db1c7ec706d93218a3b182aec91653
Chris Penner <cpenner@vendasta.com>
2018-06-04 15:52:55 -0600

    Successfully searching topics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { RepositoryListComponent } from './repository-list/repository-list.component';
-
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { RepositoryService } from './repository.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SearchBoxComponent,
-    RepositoryListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, SearchBoxComponent, RepositoryListComponent],
+  imports: [BrowserModule, MatInputModule, MatFormFieldModule, FormsModule, HttpClientModule],
+  providers: [RepositoryService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
